89f6ee4e58bec28041f9e5a5d26df3b83b990b4c
Arturs Kurzemnieks <arturs.kurzemnieks@gmail.com>
2017-06-19 17:17:20 +0300

    ..

--- classifier.py
+++ classifier.py
@@ -174,7 +174,7 @@ tests = [
 for filename in listdir("./_untagged/"):
     with open("./_untagged/" + filename, encoding='utf-8') as untagged:
         resource = json.load(untagged)
-        title = clean(resource['title'].encode('utf-8'))
+        title = clean(resource['title'])
         vec_title = pad_to_size(vectorize_title(title), max_size)
         feed_dict = {data: [vec_title]}
-        print("{} => {}".format(resource['title'], get_top_tags(prediction.eval(session=sess, feed_dict=feed_dict))))
+        print("{} => {}".format(resource['title'].encode('utf-8'), get_top_tags(prediction.eval(session=sess, feed_dict=feed_dict))))
